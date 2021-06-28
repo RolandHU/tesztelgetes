ee8a5ed58e62ac69737df71561b0979f9cc8e495
VezRoland <vezrolaa@gmail.com>
2021-06-28 12:05:31 +0200

    Update kod.js

--- kod.js
+++ kod.js
@@ -1,6 +1,7 @@
 var fnev = document.getElementById("fnev");
 var fnevClass = document.getElementsByClassName("fnev");
 var szoveg = document.getElementById("szoveg");
+var sp = document.getElementById("sp");
 var bele = false;
 fnev.addEventListener("focusin", valami);
 fnev.addEventListener("focusout", valami);
@@ -8,13 +9,33 @@ fnev.addEventListener("focusout", valami);
 function valami() {
     if (bele == false) {
         bele = true;
-        szoveg.style.color = "rgb(184, 24, 184)";
-        szoveg.style.display = "block";
-        fnev.classList.add("fnev")
+        if (fnev.value == "") {
+            sp.style.top = "11px";
+            sp.style.animation = "none";
+            sp.style.display = "block";
+            sp.style.zIndex = "1";
+            sp.style.color = "rgb(184, 24, 184)";
+            sp.style.animation = "animacio 0.2s";
+            sp.style.animationFillMode = "forwards";
+        } else {
+            sp.style.animation = "none";
+            sp.style.display = "block";
+            sp.style.top = "-8px";
+        }
     } else {
         bele = false;
-        szoveg.style.color = "gray";
-        szoveg.style.display = "none";
-        fnev.classList.remove("fnev")
+        if (fnev.value == "") {
+            sp.style.top = "-8px";
+            sp.style.animation = "none";
+            sp.style.display = "block";
+            sp.style.zIndex = "-1";
+            sp.style.color = "gray";
+            sp.style.animation = "animacio2 0.2s";
+            sp.style.animationFillMode = "forwards";
+        } else {
+            sp.style.animation = "none";
+            sp.style.display = "none";
+            sp.style.top = "11px";
+        }
     }
 }
